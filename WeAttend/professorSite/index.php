d8b3a9b22ed692ab1066260fbf5dd61d67ee9283
MortalWombat42 <alexiv42@gmail.com>
2017-10-26 17:33:45 -0400

    refactor

---
+++ WeAttend/professorSite/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <?php
+    include 'header.php';
+  ?>
+  <body>
+    <?php
+      include 'nav.php';
+    ?>
+
+    <div class="container-fluid">
+      <h1>Welcome!</h1>
+      <p>Keep track of attendence with WeAttend</p>
+    </div>
+  </body>
+</html>
