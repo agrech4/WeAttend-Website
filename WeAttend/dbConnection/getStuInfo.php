57a39eb7220a1df299b5719ee22f28e15e41ac2c
neal <neal@192.168.10.101>
2017-11-01 08:24:19 -0400

    add students' class info to json

--- WeAttend/dbConnection/getStuInfo.php
+++ WeAttend/dbConnection/getStuInfo.php
@@ -1,10 +1,3 @@
-<!--
-Author: Neal Zhu
-
-This file will take a netID as input and check if the netID belong to UVM student. 
-Then it will pull out this student's information such as first and last name 
-courses they are current taking. 
--->
 <?php
 
 $netId = '';
@@ -28,7 +21,6 @@ if (!empty($netId)) {
         // if we found a match (in this example we should actually always find just one
         if (ldap_count_entries($ds, $sr) > 0) {
             $info = ldap_get_entries($ds, $sr);
-            print_r($info);
             $returnVal->isStudent = "true";
             $returnVal->givenName = $info[0]["givenname"][0];
             $returnVal->lastName = $info[0]["sn"][0];
@@ -38,6 +30,25 @@ if (!empty($netId)) {
 }
 
 
+if($returnVal->isStudent == "true") {
+    $returnVal->classList = array();
+    include 'lib/constants.php';
+    include LIB_PATH . '/Connect-With-Database.php';
 
+    $query = "SELECT tblCourse.fldClassSubject, tblCourse.fldCourseNum, tblSections.fldSection, tblSections.fnkCourseId, fnkSectionId "
+            . "FROM tblStudentSection JOIN tblSections ON tblSections.pmkSectionId = fnkSectionId "
+            . "JOIN tblCourse ON tblCourse.pmkCourseId = tblSections.fnkCourseId "
+            . "WHERE fldStuNetId = ? "
+            . "GROUP BY tblSections.fldSection, tblSections.fnkCourseId";
+    $parameters = array($netId);
+    if ($thisDatabaseReader->querySecurityOk($query, 1, 0, 0, 0, 0)) {
+        $query = $thisDatabaseReader->sanitizeQuery($query);
+        $records = $thisDatabaseReader->select($query, $parameters);
+    }
+    foreach ($records as $record) {
+        $array = array("courseSubj" => $record["fldClassSubject"], "courseNum" => $record["fldCourseNum"], "section" => $record["fldSection"]);
+        array_push($returnVal->classList, $array);
+    }
+}
 echo json_encode($returnVal);
 ?>
